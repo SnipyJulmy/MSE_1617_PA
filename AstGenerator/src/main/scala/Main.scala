5064d621212e564bab4a59b0dbcd4b21c11cf996
Sylvain Julmy <sylvain.julmy@gmail.com>
2016-10-04 16:55:28 +0200

    Simple visualisation using graphstream (http://graphstream-project.org/)

--- AstGenerator/src/main/scala/Main.scala
+++ AstGenerator/src/main/scala/Main.scala
@@ -1,3 +1,5 @@
+import org.graphstream.graph.Graph
+import org.graphstream.graph.implementations.{DefaultGraph, MultiGraph}
 import spinal.core.SpinalConfig
 import spinal.core._
 
@@ -10,17 +12,20 @@ object Main
 {
     def main(args: Array[String])
     {
-        val report = SpinalConfig(targetDirectory = "vhdl").generateVhdl(new BasicComponent)
+        System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer")
+
+        val report = SpinalConfig(targetDirectory = "vhdl").generateVhdl(new HierarchicComponent)
+
         parseAstTree(report.toplevel)
     }
 
     def parseAstTree(root: Component): Unit =
     {
-        def parseIO(component: Component, level: Int): Unit =
+        def parseIO(component: Component, level: Int, g: Graph): Unit =
         {
             component.getAllIo.foreach
             {
-                n =>
+                n : spinal.core.BaseType =>
                 {
                     println(s"${"\t" * level}${n.toString()}")
                     if (n.isInput)
@@ -34,33 +39,58 @@ object Main
                                 tmp.toString()
                         }
 
+                        if (n.getInput(0) != null)
+                        {
+                            val label = n.getInput(0).toString()
+                            addEdge(g, component.definitionName, n.getInput(0).component.definitionName,label = label)
+                        }
+
                         println(s"${"\t" * (level + 1)}-> input from $from")
                     }
                     else if (n.isOutput)
                     {
-                        val to =
-                        {
-                            if(n.component.parent == null)
-                                "Root component output"
-                            else
-                                "???"
+                        n.getInputs.foreach
+                        { n_input =>
+                            println(s"${"\t" * (level + 1)}${n_input.toString()} nb inputs : ${n.getInputsCount}")
                         }
-                        println(s"${"\t" * (level + 1)}-> output to $to")
                     }
                 }
             }
         }
 
-        def parseComponent(crt: Component, level: Int): Unit =
+        def parseComponent(crt: Component, level: Int, g: Graph): Unit =
         {
+            val node: org.graphstream.graph.Node = g.addNode(crt.definitionName)
+            node.addAttribute("ui.label", crt.definitionName)
+
             println("\t" * level + crt.definitionName)
             println(s"${"\t" * level}{")
-            parseIO(crt, level + 1)
+            parseIO(crt, level + 1, g)
             println(s"${"\t" * level}}")
 
-            crt.children.foreach(parseComponent(_, level + 1))
+            crt.children.foreach((crt: Component) => parseComponent(crt, level + 1, g))
         }
 
-        parseComponent(root, 0)
+        def addEdge(graph: Graph, from: String, to: String, start: Int = 0, label: String = ""): Unit =
+        {
+            if (graph.getEdge(from + to + start) == null)
+            {
+                val edge: org.graphstream.graph.Edge = graph.addEdge(from + to + start.toString, to, from, true)
+                edge.addAttribute("ui.label", label)
+                edge.addAttribute("ui.style","text-background-mode : plain;")
+                //for java/scala interaction
+                //noinspection RemoveRedundantReturn
+                return
+            }
+            else
+                addEdge(graph, from, to, start = start + 1,label = label)
+
+        }
+
+        val g: Graph = new MultiGraph("Basic visualization")
+
+        parseComponent(root, 0, g)
+
+        // g.display(true)
     }
 }
