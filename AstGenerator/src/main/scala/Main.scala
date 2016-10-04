892ccf02460affc3332ec1f9d9f12a158a4a1eb3
Sylvain Julmy <sylvain.julmy@gmail.com>
2016-10-04 12:03:50 +0200

    print IO for each component

--- AstGenerator/src/main/scala/Main.scala
+++ AstGenerator/src/main/scala/Main.scala
@@ -16,9 +16,18 @@ object Main
 
     def parseAstTree(root: Component): Unit =
     {
+        def parseIO(component: Component, level : Int): Unit =
+        {
+            component.getAllIo.foreach(n => println(s"${"\t" * level}${n.toString()}"))
+        }
+
         def parseComponent(crt : Component, level : Int) : Unit =
         {
             println("\t" * level + crt.definitionName)
+            println(s"${"\t" * level }{")
+            parseIO(crt,level + 1)
+            println(s"${"\t" * level }}")
+
             crt.children.foreach(parseComponent(_,level + 1))
         }
 
