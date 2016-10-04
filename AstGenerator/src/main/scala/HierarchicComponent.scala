2bca48c67725c60def075fecb22dad5d1687f4f3
Sylvain Julmy <sylvain.julmy@gmail.com>
2016-10-04 16:54:50 +0200

    Move BasicComponent to HierarchicComponent and add a new BasicCOmponent which is more Basic...

---
+++ AstGenerator/src/main/scala/HierarchicComponent.scala
@@ -0,0 +1,97 @@
+import spinal.core._
+
+/**
+  * Created by snipy on 02.10.16.
+  */
+
+class HierarchicComponent extends Component
+{
+    val andGate = new AndGate
+    val xorGate = new XorGate
+    val andXorGate1 = new AndXor
+    val orGate = new OrGate
+    val andXorGate2 = new AndXor
+
+    val io = new Bundle
+    {
+        val a = in Bool
+        val b = in Bool
+        val c = in Bool
+        val d = out Bool
+        val e = out Bool
+    }
+
+    andGate.io.a := io.a
+    andGate.io.b := io.b
+    orGate.io.a := io.a
+    orGate.io.b := io.c
+    xorGate.io.a := io.b
+    xorGate.io.b := io.c
+
+    andXorGate1.io.a := io.a
+    andXorGate1.io.b := io.b
+
+    andXorGate2.io.a := io.a
+    andXorGate2.io.b := io.b
+
+    io.d := andGate.io.c & xorGate.io.c & orGate.io.c
+    io.e := andXorGate1.io.c | andXorGate1.io.d | andXorGate2.io.c | andXorGate2.io.d
+}
+
+class AndGate extends Component
+{
+    val io = new Bundle
+    {
+        val a = in Bool
+        val b = in Bool
+        val c = out Bool
+    }
+
+    io.c := io.a & io.b
+}
+
+class OrGate extends Component
+{
+    val io = new Bundle
+    {
+        val a = in Bool
+        val b = in Bool
+        val c = out Bool
+    }
+
+    io.c := io.a & io.b
+}
+
+class XorGate extends Component
+{
+    val io = new Bundle
+    {
+        val a = in Bool
+        val b = in Bool
+        val c = out Bool
+    }
+
+    io.c := io.a & io.b
+}
+
+class AndXor extends Component
+{
+    val xorGate = new XorGate
+    val andGate = new AndGate
+
+    val io = new Bundle
+    {
+        val a = in Bool
+        val b = in Bool
+        val c = out Bool
+        val d = out Bool
+    }
+
+    xorGate.io.a := io.a
+    xorGate.io.b := io.b
+    andGate.io.a := io.a
+    andGate.io.b := io.b
+
+    io.c := xorGate.io.c
+    io.d := andGate.io.c
+}
