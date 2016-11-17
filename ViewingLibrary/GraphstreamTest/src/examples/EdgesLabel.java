0283dfd3ce4e7a0159e08a8a11e2a7ea93adeb8e
Sylvain Julmy <sylvain.julmy@gmail.com>
2016-11-17 09:50:44 +0100

    Refactoring the location of the GraphStream test

---
+++ ViewingLibrary/GraphstreamTest/src/examples/EdgesLabel.java
@@ -0,0 +1,27 @@
+package examples;
+
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.MultiGraph;
+
+/**
+ * KlugHDL
+ * Created by snipy on 21.10.16.
+ */
+public class EdgesLabel
+{
+    public static void main(String[] args)
+    {
+        System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
+
+        Graph graph = new MultiGraph("Edges label example");
+        graph.addAttribute("ui.quality");
+        graph.addAttribute("ui.antialias");
+
+        graph.addNode("A");
+        graph.addNode("B");
+        Edge edge = graph.addEdge("AB", "A", "B", true);
+        edge.addAttribute("ui.label", "A --> B");
+        graph.display(false);
+    }
+}
---
+++ ViewingLibrary/GraphstreamTest/src/examples/SpriteEdgesExample.java
@@ -0,0 +1,46 @@
+package examples;
+
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.MultiGraph;
+import org.graphstream.ui.spriteManager.Sprite;
+import org.graphstream.ui.spriteManager.SpriteManager;
+
+/**
+ * KlugHDL
+ * Created by snipy on 21.10.16.
+ */
+public class SpriteEdgesExample
+{
+    public static void main(String[] args)
+    {
+        System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
+
+        Graph graph = new MultiGraph("Edge multiple label example");
+        graph.addAttribute("ui.quality");
+        graph.addAttribute("ui.antialias");
+
+        graph.addNode("A");
+        graph.addNode("B");
+
+        graph.addEdge("AB", "A", "B", true);
+
+        // add labels using sprites
+        SpriteManager sman = new SpriteManager(graph);
+
+        // add label 1
+        Sprite label1 = sman.addSprite("label1");
+        label1.attachToEdge("AB");
+        label1.setPosition(0.2, 0, 0);
+        label1.addAttribute("ui.label", "Label 1");
+        label1.addAttribute("ui.style", "fill-mode:none;");
+
+        // add label 2
+        Sprite label2 = sman.addSprite("label2");
+        label2.attachToEdge("AB");
+        label2.setPosition(0.8, 0, 0);
+        label2.addAttribute("ui.label", "Label 2");
+        label2.addAttribute("ui.style", "fill-mode:none;");
+
+        graph.display(true);
+    }
+}
