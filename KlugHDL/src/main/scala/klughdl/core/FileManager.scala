81419c21be026cc287cc7537c731a14df25bd6ef
Sylvain Julmy <sylvain.julmy@gmail.com>
2016-11-18 16:18:45 +0100

    Refactoring code style

--- KlugHDL/src/main/scala/klughdl/core/FileManager.scala
+++ KlugHDL/src/main/scala/klughdl/core/FileManager.scala
@@ -7,7 +7,7 @@ import java.io.{File, PrintWriter}
   * Created by snipy on 10.11.16.
   */
 case class FileManager(filename: String, directoryPath: String) {
-
+  
   private val directory: File = new File(directoryPath)
   if (!directory.exists()) {
     directory.mkdir()
@@ -15,22 +15,22 @@ case class FileManager(filename: String, directoryPath: String) {
   else if (!directory.isDirectory) {
     throw new IllegalArgumentException(s"$directoryPath is not a directory !")
   }
-
+  
   private val completeFileName = s"$directoryPath/$filename"
   private val outputFile = new File(completeFileName)
   if (!outputFile.exists()) outputFile.createNewFile()
   private val pw = new PrintWriter(outputFile)
-
+  
   def println(str: String): FileManager = {
     pw.write(str + "\n")
     this
   }
-
+  
   def print(str: String): FileManager = {
     pw.write(str)
     this
   }
-
+  
   def close(): FileManager = {
     pw.flush()
     pw.close()
--- KlugHDL/src/main/scala/klughdl/core/KlugHDLComponent.scala
+++ KlugHDL/src/main/scala/klughdl/core/KlugHDLComponent.scala
@@ -7,38 +7,36 @@ import spinal.core.Component
   * Created by snipy on 10.11.16.
   */
 class KlugHDLComponent(val name: String, val parent: Component) {
-
+  
   val id: String = if (parent == null) s"${parent}_$name" else s"${parent.definitionName}_$name"
   val idStr: String = s""""$id""""
   private var ports: Set[Port] = Set()
-
+  
   def addPort(port: Port) = {
     ports = ports + port
   }
-
-
+  
   def toJs: String = {
     val declaration = s"var $id = new ComponentShape();\n"
     val setName = s"$id.setName($idStr);\n"
     val ports = s"${generatePorts()}\n"
     val layoutXY = s"$layoutX$layoutY\n"
     val canvas = s"canvas.add($id);\n"
-
-    println(layoutXY)
-
     s"$declaration$setName$ports$layoutXY$canvas"
   }
-
+  
   def layoutX: String = s"""$id.setX(g.node("$id").x);\n"""
-
+  
   def layoutY: String = s"""$id.setY(g.node("$id").y);\n"""
-
+  
   def generatePorts(): String = {
     ports.map(p => s"""$id.addPort("${p.name}","${p.getType}");""").mkString("\n")
   }
+  
+  override def toString: String = s"$name"
 }
 
 object KlugHDLComponent {
-
+  
   def apply(name: String, parent: Component): KlugHDLComponent = new KlugHDLComponent(name, parent)
 }
