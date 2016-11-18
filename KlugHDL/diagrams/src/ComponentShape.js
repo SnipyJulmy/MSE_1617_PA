0d3eb5c6b5b536f79edcfab656e9e5a94afbec57
Sylvain Julmy <sylvain.julmy@gmail.com>
2016-11-18 12:50:24 +0100

    Add the componentShape to the klugHDL module

---
+++ KlugHDL/diagrams/src/ComponentShape.js
@@ -0,0 +1,75 @@
+/**
+ * KlugHDL
+ * Created by snipy on 09.11.16.
+ */
+
+ComponentShape = draw2d.shape.layout.VerticalLayout.extend({
+    NAME: "ComponentShape",
+
+    init: function (attr) {
+
+        var _this = this;
+
+        this._super($.extend({
+            bgColor: "#ffffff",
+            color: "#000000",
+            stroke: 1,
+            radius: 3
+        }, attr));
+
+        this.classLabel = new draw2d.shape.basic.Label({
+            text: "ClassName",
+            stroke: 1,
+            fontColor: "#000000",
+            bgColor: "#ffffff",
+            radius: this.getRadius(),
+            padding: 10,
+            resizeable: true,
+            editor: new draw2d.ui.LabelInplaceEditor(),
+            onDoubleClick: function () {
+                _this.doubleClickCallBack()
+            }
+        });
+
+        this.add(this.classLabel);
+    },
+
+    setName: function (name) {
+        this.classLabel.setText(name);
+    },
+
+    getPort: function (name) {
+        return this.getPorts().find(function (entry) {
+            return entry.name == name
+        });
+    },
+
+    addPort: function (name, type) {
+        var _this = this;
+        var label = new draw2d.shape.basic.Label({
+            text: name,
+            stroke: 0,
+            radius: 90,
+            bgColor: null,
+            padding: {left: 10, top: 3, right: 10, bottom: 5},
+            fontColor: "#000000",
+            resizeable: true,
+            onDoubleClick: function () {
+                _this.doubleClickCallBack()
+            },
+            editor: new draw2d.ui.LabelEditor()
+        });
+
+        var port = label.createPort(type);
+        port.setName(name);
+
+        this.add(label);
+
+        return label;
+    },
+
+    doubleClickCallBack: function () {
+        console.log("OK")
+    }
+
+});
