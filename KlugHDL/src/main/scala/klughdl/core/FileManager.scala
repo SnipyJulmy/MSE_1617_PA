924ce6e3a3b59e731caca1f2682977ff03d129e5
Sylvain Julmy <sylvain.julmy@gmail.com>
2016-11-21 16:22:49 +0100

    error handling + option for remove existing file

--- KlugHDL/src/main/scala/klughdl/core/FileManager.scala
+++ KlugHDL/src/main/scala/klughdl/core/FileManager.scala
@@ -2,12 +2,14 @@ package klughdl.core
 
 import java.io.{File, PrintWriter}
 
+import scala.util.{Failure, Success, Try}
+
 /**
   * KlugHDL
   * Created by snipy on 10.11.16.
   */
-case class FileManager(filename: String, directoryPath: String) {
-  
+case class FileManager(filename: String, directoryPath: String, deleteTargetBeforeRun: Boolean = false) {
+
   private val directory: File = new File(directoryPath)
   if (!directory.exists()) {
     directory.mkdir()
@@ -15,20 +17,30 @@ case class FileManager(filename: String, directoryPath: String) {
   else if (!directory.isDirectory) {
     throw new IllegalArgumentException(s"$directoryPath is not a directory !")
   }
-  
+
   private val completeFileName = s"$directoryPath/$filename"
   private val outputFile = new File(completeFileName)
-  if (!outputFile.exists()) outputFile.createNewFile()
-  private val pw = new PrintWriter(outputFile)
-  
-  def println(str: String): FileManager = {
-    pw.write(str + "\n")
-    this
+
+  if (deleteTargetBeforeRun) {
+    if (outputFile.isFile && outputFile.exists()) outputFile.delete()
   }
   
+  if (!outputFile.exists()) outputFile.createNewFile()
+  private val pw = new PrintWriter(outputFile)
+
+  def println(str: String): FileManager = print(str + "\n")
+  
   def print(str: String): FileManager = {
-    pw.write(str)
-    this
+    Try {
+      pw.write(str)
+      this
+    } match {
+      case Failure(exception) =>
+        exception.printStackTrace(System.err)
+        this
+      case Success(value) =>
+        value
+    }
   }
   
   def close(): FileManager = {
