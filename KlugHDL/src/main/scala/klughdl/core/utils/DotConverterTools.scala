29bf7485c8dc04d96310530f1ae467bf2a2233ec
Sylvain Julmy <sylvain.julmy@gmail.com>
2016-11-30 13:48:23 +0100

    Singleton for managing the conversion from the dot file to some other format like pdf or xdot

---
+++ KlugHDL/src/main/scala/klughdl/core/utils/DotConverterTools.scala
@@ -0,0 +1,63 @@
+package klughdl.core.utils
+
+import java.io.File
+
+import klughdl.core.dot.DotOutputType
+import klughdl.core.dot.DotOutputType.DotOutputType
+import klughdl.core.dot.DotProgram._
+
+import scala.sys.process._
+
+/**
+  * KlugHDL
+  * Created by snipy on 24.11.16.
+  */
+object DotConverterTools {
+  
+  /**
+    * TODO
+    * @param filepath
+    * @param overwrite
+    */
+  def generatePdfFile(filepath : String, overwrite : Boolean = true) : Unit = {
+    val srcFile = new File(filepath)
+    if (!srcFile.exists()) {
+      System.err.println(s"file $filepath does not exist")
+    } else {
+      execDotProg(srcFile, dot)
+      execDotProg(srcFile, neato)
+      execDotProg(srcFile, circo)
+      execDotProg(srcFile, fdp)
+      execDotProg(srcFile, twopi)
+    }
+  }
+  
+  /**
+    * TODO
+    * @param src
+    * @param prog
+    * @param outputType
+    */
+  private def execDotProg(src : File, prog : DotProgram = dot, outputType : DotOutputType = DotOutputType.pdf) : Unit = {
+    val outputFile = new File(s"${src.getAbsolutePath}.$prog.$outputType")
+    val query = s"$prog -T$outputType ${src.getAbsolutePath} -o ${outputFile.getAbsolutePath}"
+    println(query)
+    val result = query !!
+    
+    if (!result.isEmpty) {
+      System.err.println(result)
+    }
+  }
+  
+  /**
+    * TODO
+    * @param filepath
+    * @param overwrite
+    */
+  def generateLayoutFile(filepath : String, overwrite : Boolean = true) : Unit = {
+    ???
+  }
+}
+
+
+
