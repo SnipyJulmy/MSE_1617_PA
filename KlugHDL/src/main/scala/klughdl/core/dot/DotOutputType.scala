9c3f2ae5ecf60686deaea3114d744da7487bb541
Sylvain Julmy <sylvain.julmy@gmail.com>
2016-11-30 13:49:11 +0100

    Enum for managing the dot converter tools

---
+++ KlugHDL/src/main/scala/klughdl/core/dot/DotOutputType.scala
@@ -0,0 +1,11 @@
+package klughdl.core.dot
+
+/**
+  * KlugHDL
+  * Created by snipy on 30.11.16.
+  */
+object DotOutputType extends Enumeration {
+  
+  type DotOutputType = Value
+  val pdf, xdot, dot = Value
+}
