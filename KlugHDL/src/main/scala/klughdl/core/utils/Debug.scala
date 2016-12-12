6a749c2d5f06ae5ff06e219ce0e0a4f70e3e4e0e
Sylvain Julmy <sylvain.julmy@gmail.com>
2016-12-12 11:28:55 +0100

    Add a small debug class for dev

---
+++ KlugHDL/src/main/scala/klughdl/core/utils/Debug.scala
@@ -0,0 +1,13 @@
+package klughdl.core.utils
+
+/**
+  * KlugHDL
+  * Created by snipy on 12.12.16.
+  */
+object Debug {
+  def log(expr : => Unit) : Unit = {
+    println(s"\n${"="*10} BEGIN ${"="*10}\n")
+    expr
+    println(s"\n${"="*10}  END  ${"="*10}\n")
+  }
+}
