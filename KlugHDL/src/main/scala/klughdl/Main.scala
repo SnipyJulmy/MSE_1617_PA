679a10b99a2d2465768023bc5986fb8547727e83
Sylvain Julmy <sylvain.julmy@gmail.com>
2016-12-16 16:20:07 +0100

    Use of the json generator

--- KlugHDL/src/main/scala/klughdl/Main.scala
+++ KlugHDL/src/main/scala/klughdl/Main.scala
@@ -1,9 +1,30 @@
+/*
+ *
+ * Copyright (c) 2016  Sylvain Julmy
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 package klughdl
 
 import java.io.File
 
 import klughdl.components._
+import klughdl.core.backend.json.Json
 import klughdl.core.model.Model
+import klughdl.core.utils.FileManager
 import spinal.core._
 
 /**
@@ -20,16 +41,16 @@ object Main {
     else if (!vhdlOutputDir.isDirectory)
       System.err.println(s"$vhdlOutputDir is not a directory")
     
-    val report = SpinalConfig(targetDirectory = "vhdl").generateVhdl(new HierarchicComponent)
+    val report = SpinalConfig(targetDirectory = "vhdl").generateVhdl(new SmallComponent)
     
-    Model(report.toplevel).diagrams.foreach(d => d.generateDot(targetDirectory = "dot"))
+    // Build the model
     
-    /*
-    val contents : String = klughdl.html.index(model).toString()
+    val model = Model(report.toplevel)
     
-    FileManager("index.html", "diagrams")
-      .println(contents)
+    val json = Json().generate(model)
+    
+    FileManager("model.json", "diagrams")
+      .println(json)
       .close()
-    */
   }
 }
