78e3148ab33526afae4d1bd9d611f2f13b7021f7
Sylvain Julmy <sylvain.julmy@gmail.com>
2016-12-16 16:23:13 +0100

    add js file for the future work

--- KlugHDL/diagrams/src/main.js
+++ KlugHDL/diagrams/src/main.js
@@ -1,3 +1,17 @@
 /**
  * Created by snipy on 16.12.16.
  */
+
+function createConnection() {
+    var con = new draw2d.Connection();
+    con.setRouter(new draw2d.layout.connection.InteractiveManhattanConnectionRouter());
+    con.setTargetDecorator(new draw2d.decoration.connection.ArrowDecorator());
+    return con;
+}
+
+function newConnection(srcPort, targetPort) {
+    var c = createConnection();
+    c.setSource(srcPort);
+    c.setTarget(targetPort);
+    return c
+}
