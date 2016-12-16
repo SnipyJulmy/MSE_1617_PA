46ac3f0e4a78c176a8784411a8337471e2bba6c6
Sylvain Julmy <sylvain.julmy@gmail.com>
2016-12-16 16:13:33 +0100

    Move the dot generator to the backend package and make it inherit from the Backend trait

--- KlugHDL/src/main/scala/klughdl/core/backend/dot/Dot.scala
+++ KlugHDL/src/main/scala/klughdl/core/backend/dot/Dot.scala
@@ -1,7 +1,27 @@
-package klughdl.core.dot
+/*
+ *
+ * Copyright (c) 2016  Sylvain Julmy
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+package klughdl.core.backend.dot
 
 import java.io.File
 
+import klughdl.core.backend.Backend
 import klughdl.core.model._
 import klughdl.core.utils.{DotConverterTools, FileManager}
 
@@ -9,18 +29,25 @@ import klughdl.core.utils.{DotConverterTools, FileManager}
   * KlugHDL
   * Created by snipy on 24.11.16.
   */
-case class DotGenerator(targetDirectory : String) {
+case class Dot(targetDirectory : String) extends Backend{
   
-  def generatePDfDiagramByParent() : DotGenerator = {
+  // TODO
+  override def generate(model : Model) : String = ???
+  
+  // TODO
+  override def generate(diagram : Diagram) = ???
+  
+  def generatePDfDiagramByParent(model: Model) : Dot = {
     
     // Generate a diagram for each parent in the graph
-    Model.diagrams.foreach {
+    model.diagrams.foreach {
       generateDiagram
     }
     this
   }
   
-  def generateDiagram(diagram : Diagram) : DotGenerator = {
+  
+  def generateDiagram(diagram : Diagram) : Dot = {
     
     val extInput = "EXTERNAL_INPUT"
     val extOutput = "EXTERNAL_OUTPUT"
---
+++ KlugHDL/src/main/scala/klughdl/core/backend/dot/DotProgram.scala
@@ -0,0 +1,30 @@
+/*
+ *
+ * Copyright (c) 2016  Sylvain Julmy
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+package klughdl.core.backend.dot
+
+/**
+  * KlugHDL
+  * Created by snipy on 30.11.16.
+  */
+object DotProgram extends Enumeration {
+  
+  type DotProgram = Value
+  val dot, neato, circo, fdp, twopi = Value
+}
