759e6a428f59c9a6b1ed1e115129a61f83ba4997
Sylvain Julmy <sylvain.julmy@gmail.com>
2016-12-16 16:14:21 +0100

    add a method to generate only a diagram and not the entire model

--- KlugHDL/src/main/scala/klughdl/core/backend/Backend.scala
+++ KlugHDL/src/main/scala/klughdl/core/backend/Backend.scala
@@ -1,13 +1,29 @@
+/*
+ *
+ * Copyright (c) 2016  Sylvain Julmy
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 package klughdl.core.backend
 
-import klughdl.core.model.Model
+import klughdl.core.model.{Diagram, Model}
 
-/**
-  * Prg 1 - klughdl.core.backend
-  * Created by snipy on 16.12.16.
-  */
 trait Backend {
   
   def generate(model : Model) : String
   
+  def generate(diagram : Diagram) : String
 }
--- KlugHDL/src/main/scala/klughdl/core/backend/json/Json.scala
+++ KlugHDL/src/main/scala/klughdl/core/backend/json/Json.scala
@@ -1,5 +1,25 @@
-package klughdl.core.backend
+/*
+ *
+ * Copyright (c) 2016  Sylvain Julmy
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 
+package klughdl.core.backend.json
+
+import klughdl.core.backend.Backend
 import klughdl.core.model._
 import net.liftweb.json.JsonDSL._
 import net.liftweb.json._
@@ -11,6 +31,11 @@ case class Json() extends Backend {
   
   override def generate(model : Model) : String = {
     val json = generateJson(model)
+    pretty(render(json))
+  }
+  
+  override def generate(diagram : Diagram) : String = {
+    val json = generateJson(diagram)
     pretty(render(json))
   }
   
