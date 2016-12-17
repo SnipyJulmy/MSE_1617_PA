cfa6a40d7d646ee465a88bdbead55afa93018533
Sylvain Julmy <sylvain.julmy@gmail.com>
2016-12-17 15:52:40 +0100

    Implements TODO (generate methods)

--- KlugHDL/src/main/scala/klughdl/core/backend/dot/Dot.scala
+++ KlugHDL/src/main/scala/klughdl/core/backend/dot/Dot.scala
@@ -25,47 +25,26 @@ import klughdl.core.backend.Backend
 import klughdl.core.model._
 import klughdl.core.utils.{DotConverterTools, FileManager}
 
-/**
-  * KlugHDL
-  * Created by snipy on 24.11.16.
-  */
-case class Dot(targetDirectory : String) extends Backend{
+case class Dot(targetDirectory : String) extends Backend {
   
-  // TODO
-  override def generate(model : Model) : String = ???
+  private val extInput = "EXTERNAL_INPUT"
+  private val extOutput = "EXTERNAL_OUTPUT"
   
-  // TODO
-  override def generate(diagram : Diagram) = ???
-  
-  def generatePDfDiagramByParent(model: Model) : Dot = {
-    
-    // Generate a diagram for each parent in the graph
-    model.diagrams.foreach {
-      generateDiagram
-    }
-    this
+  override def generate(model : Model) : String = {
+    model.diagrams.map(generate).mkString("\n\n\n")
   }
   
-  
-  def generateDiagram(diagram : Diagram) : Dot = {
-    
-    val extInput = "EXTERNAL_INPUT"
-    val extOutput = "EXTERNAL_OUTPUT"
-    
-    val outputFileName = {
-      if (diagram.parent == null) s"null.dot"
-      else s"${diagram.parent.definitionName}.dot"
-    }
-    
-    val outputFile = new File(s"$targetDirectory/$outputFileName")
-    val fileManager : FileManager = FileManager(outputFileName, targetDirectory)
-    
-    // Generate basic information fot the dot diagram
-    fileManager.println("digraph g {")
-    fileManager.println("graph [rankdir=LR,ranksep=\"2\",nodesep=\"2\"];")
-    fileManager.println("node [shape=record];")
-    
-    
+  override def generate(diagram : Diagram) : String = {
+    val head =
+      s"""
+         |digraph g {
+         |graph [rankdir=LR,ranksep=\"2\",nodesep=\"2\"];
+         |node [shape=record];
+       """.stripMargin
+    val last =
+      s"""
+         |}
+       """.stripMargin
     /*
      * Now we generate :
      * - the node with port
@@ -73,39 +52,35 @@ case class Dot(targetDirectory : String) extends Backend{
      * - the outputs of the parent component which are inputs in the diagram
      */
     
-    fileManager.println {
-      diagram.components.map { entry =>
-        entry._2 match {
-          case KlugHDLComponentBasic(_, _, _) =>
-            s"""${entry._2.name} [label="{{${entry._2.inputDotPort()}}|${entry._2.name}|{${entry._2.outputDotPort()}}}"];"""
-          case KlugHDLComponentIO(name, parent) =>
-            s"""
-               |$extInput [label="{$extInput|{${entry._2.inputDotPort()}}}"];
-               |$extOutput [label="{{${entry._2.outputDotPort()}}|$extOutput}"];
+    val nodes = diagram.components.map { entry =>
+      entry._2 match {
+        case KlugHDLComponentBasic(_, _, _) =>
+          s"""${entry._2.name} [label="{{${inputDotPort(entry._2.ports)}}|${entry._2.name}|{${outputDotPort(entry._2.ports)}}}"];"""
+        case KlugHDLComponentIO(_, _) =>
+          s"""
+             |$extInput [label="{$extInput|{${inputDotPort(entry._2.ports)}}}"];
+             |$extOutput [label="{{${outputDotPort(entry._2.ports)}}|$extOutput}"];
              """.stripMargin
-        }
-      }.mkString("\n")
-    }
-  
+      }
+    }.mkString("\n")
+    
     /*
      *  Generate the connection between the nodes
      *  the connection who only consider the brother nodes
      *  are generating
      */
-    fileManager.println {
-      (for {
-        entry <- diagram.connections
-        value <- entry._2
-        if !entry._1._1.isIO && !value._1.isIO
-        if entry._1._1.component != diagram.parent
-        if entry._1._1.parent == value._1.parent
-        if entry._1._1 != value._1
-      } yield
-        s"""${entry._1._1.name}:${entry._1._2.dotName} -> ${value._1.name}:${value._2.dotName};""")
-        .toList
-        .distinct
-        .mkString("\n")
-    }
+    val connections = (for {
+      entry <- diagram.connections
+      value <- entry._2
+      if !entry._1._1.isIO && !value._1.isIO
+      if entry._1._1.component != diagram.parent
+      if entry._1._1.parent == value._1.parent
+      if entry._1._1 != value._1
+    } yield
+      s"""${entry._1._1.name}:${entry._1._2.dotName} -> ${value._1.name}:${value._2.dotName};""")
+      .toList
+      .distinct
+      .mkString("\n")
     
     /*
      * Generate the connection from the
@@ -113,7 +88,7 @@ case class Dot(targetDirectory : String) extends Backend{
      */
     
     // inputs
-    fileManager.println {
+    val inputs =
       (for {
         entry <- diagram.connections
         value <- entry._2
@@ -124,25 +99,50 @@ case class Dot(targetDirectory : String) extends Backend{
         .toList
         .distinct
         .mkString("\n")
-    }
     
     // outputs
-    fileManager.println {
-      (for {
-        entry <- diagram.connections
-        value <- entry._2
-        if value._1.isIO
-        if !entry._1._1.isIO
-      } yield
-        s"""${entry._1._1.name}:${entry._1._2.dotName} -> $extOutput:${value._2.dotName};""")
-        .toList
-        .distinct
-        .mkString("\n")
+    val outputs = (for {
+      entry <- diagram.connections
+      value <- entry._2
+      if value._1.isIO
+      if !entry._1._1.isIO
+    } yield
+      s"""${entry._1._1.name}:${entry._1._2.dotName} -> $extOutput:${value._2.dotName};""")
+      .toList
+      .distinct
+      .mkString("\n")
+    
+    head + nodes + connections + inputs + outputs + last
+  }
+  
+  def inputDotPort(ports : Set[Port]) : String = {
+    ports.filter(_.isInput).map(p => s"<${p.dotName}>${p.dotName}").mkString(" | ")
+  }
+  
+  def outputDotPort(ports : Set[Port]) : String = {
+    ports.filter(_.isOutput).map(p => s"<${p.dotName}>${p.dotName}").mkString(" | ")
+  }
+  
+  def generatePDFDiagram(model : Model) : Dot = {
+    
+    // Generate a diagram for each parent in the graph
+    model.diagrams.foreach {
+      generatePDFDiagram
+    }
+    this
+  }
+  
+  def generatePDFDiagram(diagram : Diagram) : Dot = {
+    val outputFileName = {
+      if (diagram.parent == null) s"null.dot"
+      else s"${diagram.parent.definitionName}.dot"
     }
     
-    fileManager.println("}")
-    fileManager.close()
+    val outputFile = new File(s"$targetDirectory/$outputFileName")
+    val fileManager : FileManager = FileManager(outputFileName, targetDirectory)
     DotConverterTools.generatePdfFile(outputFile.getAbsolutePath)
+    fileManager.println(generate(diagram))
+    fileManager.close()
     this
   }
 }
